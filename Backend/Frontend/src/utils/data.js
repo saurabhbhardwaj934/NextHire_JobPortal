4c06fbdc6ba34f56b8cbe62780efd4743617e9db
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-06-08 15:24:45 +0530

    final

--- Backend/Frontend/src/utils/data.js
+++ Backend/Frontend/src/utils/data.js
@@ -1,11 +1,11 @@
 export const USER_API_ENDPOINT =
-  "https://nexthire-jobportal-1-9bnb.onrender.com/api/user";
+  "https://nexthire-jobportal-2-8hfc.onrender.com/api/user";
 
 export const JOB_API_ENDPOINT =
-  "https://nexthire-jobportal-1-9bnb.onrender.com/api/job";
+  "https://nexthire-jobportal-2-8hfc.onrender.com/api/job";
 
 export const APPLICATION_API_ENDPOINT =
-  "https://nexthire-jobportal-1-9bnb.onrender.com/api/application";
+  "https://nexthire-jobportal-2-8hfc.onrender.com/api/application";
 
 export const COMPANY_API_ENDPOINT =
-  "https://nexthire-jobportal-1-9bnb.onrender.com/api/company";
+  "https://nexthire-jobportal-2-8hfc.onrender.com/api/company";
