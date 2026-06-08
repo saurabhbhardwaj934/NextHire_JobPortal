8360d1200be0d90bca10262e68faaad1ef409e91
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-06-08 16:51:50 +0530

    add vercel rewrites

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,11 +1,11 @@
 export const USER_API_ENDPOINT =
-  "https://nexthire-jobportal-2-8hfc.onrender.com/api/user";
+  "https://nexthire-zl6y.onrender.com/api/user";
 
 export const JOB_API_ENDPOINT =
-  "https://nexthire-jobportal-2-8hfc.onrender.com/api/job";
+  "https://nexthire-zl6y.onrender.com/api/job";
 
 export const APPLICATION_API_ENDPOINT =
-  "https://nexthire-jobportal-2-8hfc.onrender.com/api/application";
+  "https://nexthire-zl6y.onrender.com/api/application";
 
 export const COMPANY_API_ENDPOINT =
-  "https://nexthire-jobportal-2-8hfc.onrender.com/api/company";
+  "https://nexthire-zl6y.onrender.com/api/company";
